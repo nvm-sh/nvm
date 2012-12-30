772cb6fa8002d462eabbbd48ff6f0a18b7cd1695
munkkya <hiroyam@gmail.com>
2012-12-31 05:10:30 +0900

    fixed nvm_ls_remote using raw grep

--- nvm.sh
+++ nvm.sh
@@ -83,8 +83,8 @@ nvm_ls_remote()
         PATTERN=".*"
     fi
     VERSIONS=`curl -s http://nodejs.org/dist/ \
-                  | egrep -o 'v[0-9]+\.[0-9]+\.[0-9]+' \
-                  | grep -w "${PATTERN}" \
+                  | command egrep -o 'v[0-9]+\.[0-9]+\.[0-9]+' \
+                  | command grep -w "${PATTERN}" \
                   | sort -t. -u -k 1.2,1n -k 2,2n -k 3,3n`
     if [ ! "$VERSIONS" ]; then
         echo "N/A"
