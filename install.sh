e6116d96abb6551ed4c77e6713a7fd7ca24ff3b6
Jordan Harband <ljharb@gmail.com>
2013-09-21 12:58:01 -0700

    Mentioning `install-gitless.sh` in the "no git" error message - finishing off #246.

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 NVM_DIR="$HOME/.nvm"
 
 if ! hash git 2>/dev/null; then
-  echo >&2 "You need to install git."
-  echo >&2 "Visit http://git-scm.com/downloads"
+  echo >&2 "You need to install git - visit http://git-scm.com/downloads"
+  echo >&2 "or, use install-gitless.sh instead."
   exit 1
 fi
 
