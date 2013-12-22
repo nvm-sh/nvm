ca9bcb8ce41533378a7971d8e576210007b58956
Koen Punt <koen@koenpunt.nl>
2013-12-22 18:03:06 +0100

    Updated source string, use hard path as NVM is not always installed in `$HOME/.nvm`

--- install-gitless.sh
+++ install-gitless.sh
@@ -31,7 +31,7 @@ if [ ! "$PROFILE" ]; then
   fi
 fi
 
-SOURCE_STR="[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # This loads NVM"
+SOURCE_STR="[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"  # This loads NVM"
 
 if [ -z "$PROFILE" ] || [ ! -f "$PROFILE" ] ; then
   if [ -z $PROFILE ]; then
