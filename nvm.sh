ca0c8a7f1db78a60aed6e4e6ea1119d827f8e363
Fraser Tweedale <frase@frase.id.au>
2014-02-21 22:10:17 +1000

    handle echo implementations that do not support `-e`

--- nvm.sh
+++ nvm.sh
@@ -26,6 +26,9 @@ if [ ! -d "$NVM_DIR" ]; then
   fi
 fi
 
+DASH_E=""
+[ -z "$(echo -n -e)" ] && DASH_E="-e"
+
 # Setup mirror location if not already set
 if [ -z "$NVM_NODEJS_ORG_MIRROR" ]; then
   export NVM_NODEJS_ORG_MIRROR="http://nodejs.org/dist"
@@ -139,7 +142,7 @@ nvm_checksum() {
 }
 
 colorize_version() {
-  echo -e "\033[0;34m$1\033[0m"
+  echo $DASH_E "\033[0;34m$1\033[0m"
 }
 
 print_versions() {
@@ -482,7 +485,7 @@ nvm() {
         return
     ;;
     "current" )
-      echo -ne "current: \t"; nvm_version current
+      echo $DASH_E -n "current: \t"; nvm_version current
     ;;
     "alias" )
       mkdir -p $NVM_DIR/alias
