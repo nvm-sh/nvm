e3b84499c26564635d928e83668464e2515e3d30
Koen Punt <koen@koenpunt.nl>
2014-05-07 10:47:24 +0200

    use regex triplet, add unit test

--- nvm.sh
+++ nvm.sh
@@ -98,7 +98,7 @@ nvm_format_version() {
 }
 
 nvm_strip_path() {
-  echo "$1" | sed -e "s#$NVM_DIR/[^/]*$2[^:]*##g" | sed -e "s/::/:/g" | sed -e "s/:$//g"
+  echo "$1" | sed -e "s#$NVM_DIR/[^/]*$2[^:]*:##g" -e "s#:$NVM_DIR/[^/]*$2[^:]*##g" -e "s#$NVM_DIR/[^/]*$2[^:]*##g"
 }
 
 nvm_binary_available() {
