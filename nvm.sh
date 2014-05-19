480f58086664a6449c643fdf4cf70fe099fbae39
Marcin Jekot <marcin@jekot.net>
2014-05-19 17:28:35 +0200

    default $PATTERN to "v" correctly

--- nvm.sh
+++ nvm.sh
@@ -117,7 +117,7 @@ nvm_binary_available() {
 }
 
 nvm_ls() {
-  local PATTERN=`expr "$1" : "v"`
+  local PATTERN=${1:-v}
   local VERSIONS=''
   if [ "$PATTERN" = 'current' ]; then
     echo `node -v 2>/dev/null`
