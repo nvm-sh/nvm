578a601b2702bd1cae43f0cbc68a42849809a85c
Jordan Harband <ljharb@gmail.com>
2014-05-20 10:36:12 -0700

    Filter out of `nvm ls` things that start with a dot. Fixes #421, closes #422.

--- nvm.sh
+++ nvm.sh
@@ -133,7 +133,7 @@ nvm_ls() {
     VERSIONS="$PATTERN"
   else
     VERSIONS=`find "$NVM_DIR/" -maxdepth 1 -type d -name "$(nvm_format_version $PATTERN)*" -exec basename '{}' ';' \
-      | sort -t. -u -k 1.2,1n -k 2,2n -k 3,3n`
+      | sort -t. -u -k 1.2,1n -k 2,2n -k 3,3n | grep -v '^ *\.'`
   fi
   if [ -z "$VERSIONS" ]; then
       echo "N/A"
