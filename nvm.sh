ff1781b95d156b327977681f4a469d2e786c9770
Jordan Harband <ljharb@gmail.com>
2014-09-05 00:42:59 -0700

    Make `nvm copy-packages` refuse to copy packages from the currently activated version.

--- nvm.sh
+++ nvm.sh
@@ -792,9 +792,17 @@ nvm() {
         nvm help
         return 127
       fi
-      VERSION="$(nvm_version "$2")"
 
-      # declare local INSTALLS first, otherwise it doesn't work in zsh
+      local PROVIDED_VERSION
+      PROVIDED_VERSION="$2"
+
+      if [ "$PROVIDED_VERSION" = "$(nvm_ls_current)" ]; then
+        echo 'Can not copy packages from the current version of node.' >&2
+        return 2
+      fi
+
+      local VERSION
+      VERSION="$(nvm_version "$PROVIDED_VERSION")"
       local INSTALLS
       INSTALLS=$(nvm use "$VERSION" > /dev/null && npm list -g --depth=0 | tail -n +2 | \grep -o -e ' [^@]*' | cut -c 2- | \grep -v npm | xargs)
 
