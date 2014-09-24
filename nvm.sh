53f9841c6c9bcf096fd3a04ae942308d792cbea6
Jordan Harband <ljharb@gmail.com>
2014-09-24 14:58:07 -0700

    v0.17.0

--- nvm.sh
+++ nvm.sh
@@ -883,7 +883,7 @@ nvm() {
       nvm_version $2
     ;;
     "--version" )
-      echo "0.16.1"
+      echo "0.17.0"
     ;;
     "unload" )
       unset -f nvm nvm_print_versions nvm_checksum nvm_ls_remote nvm_ls nvm_remote_version nvm_version nvm_rc_version nvm_version_greater nvm_version_greater_than_or_equal_to > /dev/null 2>&1
