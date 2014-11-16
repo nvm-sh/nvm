78069717f76070c6cb2587c1cfcadeeb069751ea
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2014-11-16 15:28:16 +0100

    Revert misplaced commit in 'Include actions for fish when installing' The DIR variable is used in another branch (vagrant) and doesn't belong here

--- install.sh
+++ install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Get this script after dereferincing all symlinks
-# !! Doesn't check for circular symlinks !!
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
 set -e
 
 nvm_has() {
