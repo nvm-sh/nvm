d0617b5b795a75bc66e1a05f5451dc62f9fc40df
Jordan Harband <ljharb@gmail.com>
2015-02-10 11:04:00 -0800

    Suppressing extraneous output from #622.

--- nvm.sh
+++ nvm.sh
@@ -1491,7 +1491,7 @@ $NVM_LS_REMOTE_IOJS_OUTPUT" | command grep -v "N/A" | sed '/^$/d')"
       if [ $? -ne 0 ]; then
         echo "! WARNING: Version '$3' does not exist." >&2
       fi
-      echo "$3" | tee "$NVM_ALIAS_DIR/$2"
+      echo "$3" | tee "$NVM_ALIAS_DIR/$2" >/dev/null
       if [ ! "_$3" = "_$VERSION" ]; then
         echo "$2 -> $3 (-> $VERSION)"
       else
