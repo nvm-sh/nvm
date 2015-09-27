8aebf863294ef3a21e2790c01af16405213381de
Jordan Harband <ljharb@gmail.com>
2015-09-27 06:40:29 -0700

    v0.27.1

--- nvm.sh
+++ nvm.sh
@@ -2072,7 +2072,7 @@ $NVM_LS_REMOTE_POST_MERGED_OUTPUT" | command grep -v "N/A" | command sed '/^$/d'
       nvm_remote_version "$2"
     ;;
     "--version" )
-      echo "0.27.0"
+      echo "0.27.1"
     ;;
     "unload" )
       unset -f nvm nvm_print_versions nvm_checksum \
