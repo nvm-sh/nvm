96afff7af2c3bd20bc9d0aa610ba888b20f163ea
Jordan Harband <ljharb@gmail.com>
2016-04-19 22:49:49 -0700

    [Tests] Set up “mocks” directory, and updater script.

---
+++ update_test_mocks.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo 'Updating test mocks...'
+
+MOCKS_DIR="$PWD/test/fast/Unit tests/mocks"
+
+echo "creating $MOCKS_DIR"
+mkdir -p MOCKS_DIR
+
+. "$NVM_DIR/nvm.sh"
+
+nvm_download -L -s "$NVM_NODEJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/nodejs.org-dist-index.tab"
+nvm_download -L -s "$NVM_IOJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/iojs.org-dist-index.tab"
+
+echo "done! Don't forget to git commit them."
