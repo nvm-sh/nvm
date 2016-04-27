ec54c925c13ae4b5469a5b6cb965fa8018c32b45
Jordan Harband <ljharb@gmail.com>
2016-04-27 01:25:56 -0700

    [Tests] use mocks for `nvm_ls_remote`/`nvm_ls_remote_iojs` tests.

--- update_test_mocks.sh
+++ update_test_mocks.sh
@@ -11,6 +11,8 @@ mkdir -p MOCKS_DIR
 
 . "$NVM_DIR/nvm.sh"
 
+nvm_ls_remote > "$MOCKS_DIR/nvm_ls_remote.txt"
+nvm_ls_remote_iojs > "$MOCKS_DIR/nvm_ls_remote_iojs.txt"
 nvm_download -L -s "$NVM_NODEJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/nodejs.org-dist-index.tab"
 nvm_download -L -s "$NVM_IOJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/iojs.org-dist-index.tab"
 
