d2744014df3a5009d7c1cbd67276008543976d31
Jordan Harband <ljharb@gmail.com>
2016-06-25 17:00:19 -0700

    [Tests] add some basic `nvm ls-remote` unit tests.

--- update_test_mocks.sh
+++ update_test_mocks.sh
@@ -10,6 +10,10 @@ echo "creating $MOCKS_DIR"
 mkdir -p "$MOCKS_DIR"
 
 . "$NVM_DIR/nvm.sh" --no-use
+nvm deactivate 2> /dev/null
+nvm_is_version_installed() {
+  return 1
+}
 
 nvm_ls_remote > "$MOCKS_DIR/nvm_ls_remote.txt"
 nvm_ls_remote_iojs > "$MOCKS_DIR/nvm_ls_remote_iojs.txt"
@@ -17,5 +21,9 @@ NVM_LTS=* nvm_ls_remote > "$MOCKS_DIR/nvm_ls_remote LTS.txt"
 NVM_LTS=argon nvm_ls_remote > "$MOCKS_DIR/nvm_ls_remote LTS argon.txt"
 nvm_download -L -s "$NVM_NODEJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/nodejs.org-dist-index.tab"
 nvm_download -L -s "$NVM_IOJS_ORG_MIRROR/index.tab" -o - > "$MOCKS_DIR/iojs.org-dist-index.tab"
+nvm ls-remote > "$MOCKS_DIR/nvm ls-remote.txt"
+nvm ls-remote --lts > "$MOCKS_DIR/nvm ls-remote lts.txt"
+nvm ls-remote node > "$MOCKS_DIR/nvm ls-remote node.txt"
+nvm ls-remote iojs > "$MOCKS_DIR/nvm ls-remote iojs.txt"
 
 echo "done! Don't forget to git commit them."
