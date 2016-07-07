186eb8838dd43df3c3bf7b9d301101a9177398ca
Jordan Harband <ljharb@gmail.com>
2016-07-07 15:38:34 -0700

    [Tests] make “nvm use iojs” test actually test that thing.

--- test/common.sh
+++ test/common.sh
@@ -17,3 +17,49 @@ strip_colors() {
     echo "$line" | LC_ALL=C command sed 's/\[[ -?]*[@-~]//g'
   done
 }
+
+make_echo() {
+  echo "#!/bin/sh" > "$1"
+  echo "echo \"${2}\"" > "$1"
+  chmod a+x "$1"
+}
+
+make_fake_node() {
+  local VERSION
+  VERSION="${1-}"
+  [ -n "${VERSION}" ] || return 1
+
+  local BIN_PATH
+  BIN_PATH="$(nvm_version_path "${VERSION}")/bin"
+  mkdir -p "${BIN_PATH}" || {
+    echo >&2 'unable to make bin dir'
+    return 2
+  }
+
+  make_echo "${BIN_PATH}/node" "${VERSION}" || {
+    echo >&2 'unable to make fake node bin'
+    return 3
+  }
+}
+
+make_fake_iojs() {
+  local VERSION
+  VERSION="${1-}"
+  [ -n "${VERSION}" ] || return 1
+
+  local BIN_PATH
+  BIN_PATH="$(nvm_version_path "iojs-${VERSION}")/bin"
+  mkdir -p "${BIN_PATH}" || {
+    echo >&2 'unable to make bin dir'
+    return 2
+  }
+
+  make_echo "${BIN_PATH}/node" "${VERSION}" || {
+    echo >&2 'unable to make fake node bin'
+    return 3
+  }
+  make_echo "${BIN_PATH}/iojs" "${VERSION}" || {
+    echo >&2 'unable to make fake iojs bin'
+    return 3
+  }
+}
