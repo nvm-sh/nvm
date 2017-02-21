6669e91bd8187ae9efbf110e31fc4e4d7b79011f
Jordan Harband <ljharb@gmail.com>
2017-02-20 17:59:20 -0800

    v0.33.1

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ nvm_install_dir() {
 }
 
 nvm_latest_version() {
-  echo "v0.33.0"
+  echo "v0.33.1"
 }
 
 #
--- nvm.sh
+++ nvm.sh
@@ -3165,7 +3165,7 @@ nvm() {
       NVM_VERSION_ONLY=true NVM_LTS="${NVM_LTS-}" nvm_remote_version "${PATTERN:-node}"
     ;;
     "--version" )
-      nvm_echo '0.33.0'
+      nvm_echo '0.33.1'
     ;;
     "unload" )
       nvm deactivate >/dev/null 2>&1
