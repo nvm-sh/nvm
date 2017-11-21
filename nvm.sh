16d3e6d2dce4c38de0acfe0a51fd142c9a8e9b3b
mail6543210 <mail6543210@yahoo.com.tw>
2017-11-21 23:44:38 +0800

    Remove unnecessary, incorrect workaround.
    The "\printf" calls zsh shell builtin instead of `command printf` in scripting.
    
    The workaround is no longer needed given 91a29c0.

--- nvm.sh
+++ nvm.sh
@@ -13,13 +13,7 @@
 NVM_SCRIPT_SOURCE="$_"
 
 nvm_echo() {
-  command printf %s\\n "$*" 2>/dev/null || {
-    nvm_echo() {
-      # shellcheck disable=SC1001
-      \printf %s\\n "$*" # on zsh, `command printf` sometimes fails
-    }
-    nvm_echo "$@"
-  }
+  command printf %s\\n "$*" 2>/dev/null
 }
 
 nvm_cd() {
