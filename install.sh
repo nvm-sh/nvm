8debf39f24954d1006df6bac2dbef09d1ea68bc5
Jordan Harband <ljharb@gmail.com>
2020-09-23 16:18:37 -0700

    v0.36.0

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ nvm_install_dir() {
 }
 
 nvm_latest_version() {
-  echo "v0.35.3"
+  echo "v0.36.0"
 }
 
 nvm_profile_is_bash_or_zsh() {
--- nvm.sh
+++ nvm.sh
@@ -3647,7 +3647,7 @@ nvm() {
       NVM_VERSION_ONLY=true NVM_LTS="${NVM_LTS-}" nvm_remote_version "${PATTERN:-node}"
     ;;
     "--version" | "-v")
-      nvm_echo '0.35.3'
+      nvm_echo '0.36.0'
     ;;
     "unload")
       nvm deactivate >/dev/null 2>&1
