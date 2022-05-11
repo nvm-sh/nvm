8d536dc50089e019927b843bf9f0b2aea94eff09
immeëmosol <will+developer@willfris.nl>
2022-05-11 14:07:04 +0200

    Allow empty or unset NVM_ENV in install.sh

--- install.sh
+++ install.sh
@@ -470,6 +470,6 @@ nvm_reset() {
     nvm_do_install nvm_reset nvm_default_install_dir nvm_grep
 }
 
-[ "_$NVM_ENV" = "_testing" ] || nvm_do_install
+[ "_${NVM_ENV-}" = "_testing" ] || nvm_do_install
 
 } # this ensures the entire script is downloaded #
