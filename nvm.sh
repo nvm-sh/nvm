8fbf8ab6943d0bcb402773c3c2b30c2f103d7fa2
Jordan Harband <ljharb@gmail.com>
2023-07-24 18:29:54 -0700

    v0.39.4

--- nvm.sh
+++ nvm.sh
@@ -4173,7 +4173,7 @@ nvm() {
       NVM_VERSION_ONLY=true NVM_LTS="${NVM_LTS-}" nvm_remote_version "${PATTERN:-node}"
     ;;
     "--version" | "-v")
-      nvm_echo '0.39.3'
+      nvm_echo '0.39.4'
     ;;
     "unload")
       nvm deactivate >/dev/null 2>&1
