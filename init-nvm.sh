1ffec06f0e48fe67415e213f2cfa38a219b29e80
carlosd <60242467+carlosdem@users.noreply.github.com>
2023-09-03 10:10:09 +1000

    Create init-nvm.sh
    take from arch packaging

---
+++ init-nvm.sh
@@ -0,0 +1,6 @@
+# Maintainer: Tom Wadley <tom@tomwadley.net>
+
+[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+source /usr/share/nvm/nvm.sh
+source /usr/share/nvm/bash_completion
+source /usr/share/nvm/install-nvm-exec
