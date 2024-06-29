a2996f3b0efbe3e15e901edb17e03e8310cb0980
ryenus <ryenus@gmail.com>
2024-06-30 01:50:15 +0800

    rename NVM_MIN_VER to NVM_MIN
    since nvm is all about versions, so no need for the explicit suffix.

--- nvm.sh
+++ nvm.sh
@@ -1856,7 +1856,7 @@ nvm_print_versions() {
   fi
 
   command awk \
-    -v remote_versions="$(printf '%s' "${1-}" | tr '\n' '|')" -v min_ver="${NVM_MIN_VER:-v0}" \
+    -v remote_versions="$(printf '%s' "${1-}" | tr '\n' '|')" -v min_ver="${NVM_MIN:-v0}" \
     -v installed_versions="$(nvm_ls | tr '\n' '|')" -v current="$NVM_CURRENT" \
     -v installed_color="$INSTALLED_COLOR" -v system_color="$SYSTEM_COLOR" \
     -v current_color="$CURRENT_COLOR" -v default_color="$DEFAULT_COLOR" \
