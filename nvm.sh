301cfa12d614289297c9c17240ae0993e73c75d2
ryenus <ryenus@gmail.com>
2024-07-15 14:45:57 +0800

    avoid inline initialization for ksh compatibility
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

--- nvm.sh
+++ nvm.sh
@@ -4135,7 +4135,8 @@ nvm() {
     ;;
     "ls-remote" | "list-remote")
       local NVM_LTS
-      local NVM_MIN="${NVM_MIN-}"
+      local NVM_MIN
+      NVM_MIN="${NVM_MIN-}"
       local PATTERN
       local NVM_NO_COLORS
 
