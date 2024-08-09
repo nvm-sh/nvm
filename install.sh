81f18bc4450ec744fa594486f2c4330b109e21a5
Jordan Harband <ljharb@gmail.com>
2024-08-09 13:46:16 +1200

    [patch] `install.sh`: fix node capitalization

--- install.sh
+++ install.sh
@@ -358,7 +358,7 @@ nvm_check_global_modules() {
     command printf %s\\n "$NPM_GLOBAL_MODULES"
     nvm_echo '=> If you wish to uninstall them at a later point (or re-install them under your'
     # shellcheck disable=SC2016
-    nvm_echo '=> `nvm` Nodes), you can remove them from the system Node as follows:'
+    nvm_echo '=> `nvm` node installs), you can remove them from the system Node as follows:'
     nvm_echo
     nvm_echo '     $ nvm use system'
     nvm_echo '     $ npm uninstall -g a_module'
