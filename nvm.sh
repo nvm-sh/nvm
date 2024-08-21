nvm-sh/nvm

commit c31a867c463bbe2251bd3b996a611f8cdc3007eb

[Fix] `--save`: avoid a conflict with `set -e`
Fixes #3405.

nvm.sh
@@ -4556,31 +4556,41 @@ nvm_supports_xz() {
 nvm_auto() {
   local NVM_MODE
   NVM_MODE="${1-}"
-  local VERSION
-  local NVM_CURRENT
-  if [ "_${NVM_MODE}" = '_install' ]; then
-    VERSION="$(nvm_alias default 2>/dev/null || nvm_echo)"
-    if [ -n "${VERSION}" ] && ! [ "_${VERSION}" = '_N/A' ] && nvm_is_valid_version "${VERSION}"; then
-      nvm install "${VERSION}" >/dev/null
-    elif nvm_rc_version >/dev/null 2>&1; then
-      nvm install >/dev/null
-    fi
-  elif [ "_$NVM_MODE" = '_use' ]; then
-    NVM_CURRENT="$(nvm_ls_current)"
-    if [ "_${NVM_CURRENT}" = '_none' ] || [ "_${NVM_CURRENT}" = '_system' ]; then
-      VERSION="$(nvm_resolve_local_alias default 2>/dev/null || nvm_echo)"
-      if [ -n "${VERSION}" ] && ! [ "_${VERSION}" = '_N/A' ] && nvm_is_valid_version "${VERSION}"; then
-        nvm use --silent "${VERSION}" >/dev/null
-      elif nvm_rc_version >/dev/null 2>&1; then
-        nvm use --silent >/dev/null
+
+  case "${NVM_MODE}" in
+    none) return 0 ;;
+    use | install)
+      local VERSION
+      local NVM_CURRENT
+      NVM_CURRENT="$(nvm_ls_current)"
+      if [ "_${NVM_CURRENT}" = '_none' ] || [ "_${NVM_CURRENT}" = '_system' ]; then
+        VERSION="$(nvm_resolve_local_alias default 2>/dev/null || nvm_echo)"
+        if [ -n "${VERSION}" ]; then
+          if [ "_${VERSION}" != '_N/A' ] && nvm_is_valid_version "${VERSION}"; then
+            if [ "_${NVM_MODE}" = '_install' ]; then
+              nvm install "${VERSION}" >/dev/null
+            else
+              nvm use --silent "${VERSION}" >/dev/null
+            fi
+          else
+            return 0
+          fi
+        elif nvm_rc_version >/dev/null 2>&1; then
+          if [ "_${NVM_MODE}" = '_install' ]; then
+            nvm install >/dev/null
+          else
+            nvm use --silent >/dev/null
+          fi
+        fi
+      else
+        nvm use --silent "${NVM_CURRENT}" >/dev/null
       fi
-    else
-      nvm use --silent "${NVM_CURRENT}" >/dev/null
-    fi
-  elif [ "_${NVM_MODE}" != '_none' ]; then
-    nvm_err 'Invalid auto mode supplied.'
-    return 1
-  fi
+    ;;
+    *)
+      nvm_err 'Invalid auto mode supplied.'
+      return 1
+    ;;
+  esac
 }
 
 nvm_process_parameters() {

.github/workflows/nvm-install-test.yml
@@ -1,0 +1,104 @@
+name: 'Tests: nvm install with set -e'
+
+on:
+  pull_request:
+  push:
+  workflow_dispatch:
+    inputs:
+      ref:
+        description: 'git ref to use'
+        required: false
+        default: 'HEAD'
+
+jobs:
+  matrix:
+    runs-on: ubuntu-latest
+    outputs:
+      matrix: ${{ steps.matrix.outputs.matrix }}
+    steps:
+      - uses: actions/checkout@v4
+        with:
+          fetch-depth: 0
+      - id: matrix
+        run: |
+          if [ "${{ github.event_name }}" == "workflow_dispatch" ] && [ -n "${{ github.event.inputs.ref }}" ]; then
+            echo "matrix=\"[\"${{ github.event.inputs.ref }}\"]\"" >> $GITHUB_OUTPUT
+          else
+            TAGS="$((echo "HEAD" && git tag --sort=-v:refname --merged HEAD --format='%(refname:strip=2) %(creatordate:short)' | grep '^v' | while read tag date; do
+              if [ "$(uname)" == "Darwin" ]; then
+                timestamp=$(date -j -f "%Y-%m-%d" "$date" +%s)
+                threshold=$(date -j -v-4y +%s)
+              else
+                timestamp=$(date -d "$date" +%s)
+                threshold=$(date -d "4 years ago" +%s)
+              fi
+              if [ $timestamp -ge $threshold ]; then echo "$tag"; fi
+            done) | xargs)"
+            echo $TAGS
+            TAGS_JSON=$(printf "%s\n" $TAGS | jq -R . | jq -sc .)
+            echo "matrix=${TAGS_JSON}" >> $GITHUB_OUTPUT
+          fi
+
+  test:
+    needs: [matrix]
+    runs-on: ubuntu-latest
+    continue-on-error: ${{ matrix.ref == 'v0.40.0' }} # https://github.com/nvm-sh/nvm/issues/3405
+    strategy:
+      fail-fast: false
+      matrix:
+        ref: ${{ fromJson(needs.matrix.outputs.matrix) }}
+        has-nvmrc:
+          - 'no nvmrc'
+          - 'nvmrc'
+        shell-level:
+          - 1 shlvl
+          - 2 shlvls
+
+    steps:
+      - uses: actions/checkout@v4
+      - name: resolve HEAD to sha
+        run: |
+          if [ '${{ matrix.ref }}' = 'HEAD' ]; then
+            REF="$(git rev-parse HEAD)"
+          else
+            REF="${{ matrix.ref }}"
+          fi
+          echo "resolved ref: ${REF}"
+          echo "ref="$REF"" >> $GITHUB_ENV
+      - run: echo $- # which options are set
+      - run: echo node > .nvmrc
+        if: ${{ matrix.has-nvmrc == 'nvmrc' }}
+      - run:  curl -I --compressed -v https://nodejs.org/dist/
+      - name: 'install nvm'
+        run: |
+          set -e
+          export NVM_INSTALL_VERSION="${ref}"
+          curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${ref}/install.sh" | bash
+      - name: nvm --version
+        run: |
+          set +e
+          . $NVM_DIR/nvm.sh && nvm --version
+      - name: nvm install in 1 shell level, ${{ matrix.has-nvmrc }}
+        if: ${{ matrix.shell-level == '1 shlvl' }}
+        run: |
+          set -ex
+          . $NVM_DIR/nvm.sh
+          echo nvm.sh sourced
+          nvm --version
+          if [ '${{ matrix.has-nvmrc }}' == 'nvmrc' ]; then
+            nvm install
+          fi
+      - name: nvm install in 2 shell levels, ${{ matrix.has-nvmrc }}
+        if: ${{ matrix.shell-level == '2 shlvls' }}
+        run: |
+          if [ '${{ matrix.has-nvmrc }}' == 'nvmrc' ]; then
+            bash -c "set -ex && . $NVM_DIR/nvm.sh && echo nvm.sh sourced && nvm --version && nvm install"
+          else
+            bash -c "set -ex && . $NVM_DIR/nvm.sh && echo nvm.sh sourced && nvm --version"
+          fi
+
+  finisher:
+    runs-on: ubuntu-latest
+    needs: [test]
+    steps:
+      - run: true

commit df7053524889547589211fa35e09531861b18f8e

[Fix] `nvm_auto`: Fix bug introduced in c31a867

nvm.sh
@@ -4559,7 +4559,7 @@ nvm_auto() {
 
   case "${NVM_MODE}" in
     none) return 0 ;;
-    use | install)
+    use)
       local VERSION
       local NVM_CURRENT
       NVM_CURRENT="$(nvm_ls_current)"
@@ -4567,23 +4567,26 @@ nvm_auto() {
         VERSION="$(nvm_resolve_local_alias default 2>/dev/null || nvm_echo)"
         if [ -n "${VERSION}" ]; then
           if [ "_${VERSION}" != '_N/A' ] && nvm_is_valid_version "${VERSION}"; then
-            if [ "_${NVM_MODE}" = '_install' ]; then
-              nvm install "${VERSION}" >/dev/null
-            else
-              nvm use --silent "${VERSION}" >/dev/null
-            fi
+            nvm use --silent "${VERSION}" >/dev/null
           else
             return 0
           fi
         elif nvm_rc_version >/dev/null 2>&1; then
-          if [ "_${NVM_MODE}" = '_install' ]; then
-            nvm install >/dev/null
-          else
-            nvm use --silent >/dev/null
-          fi
+          nvm use --silent >/dev/null
         fi
       else
         nvm use --silent "${NVM_CURRENT}" >/dev/null
+      fi
+    ;;
+    install)
+      local VERSION
+      VERSION="$(nvm_alias default 2>/dev/null || nvm_echo)"
+      if [ -n "${VERSION}" ] && [ "_${VERSION}" != '_N/A' ] && nvm_is_valid_version "${VERSION}"; then
+        nvm install "${VERSION}" >/dev/null
+      elif nvm_rc_version >/dev/null 2>&1; then
+        nvm install >/dev/null
+      else
+        return 0
       fi
     ;;
     *)

test/sourcing/Sourcing nvm.sh should keep version if one is active
@@ -9,12 +9,12 @@ echo '0.10.1' > ../../alias/default || die "couldn't create default alias"
 NVM_TARGET=0.10.3
 NVM_DEFAULT="$(nvm_resolve_local_alias default)"
 
-[ "_$NVM_DEFAULT" != "_$NVM_TARGET" ] || die "default $NVM_DEFAULT is the same as target $NVM_TARGET"
+[ "_${NVM_DEFAULT}" != "_${NVM_TARGET}" ] || die "default ${NVM_DEFAULT} is the same as target ${NVM_TARGET}"
 
-nvm use $NVM_TARGET || die "nvm use $NVM_TARGET failed"
+nvm use "${NVM_TARGET}" || die "nvm use ${NVM_TARGET} failed"
 
 \. ../../nvm.sh || die 'sourcing returned nonzero exit code'
 
 NVM_CURRENT="$(nvm current)"
 
-[ "_${NVM_CURRENT#v}" = "_$NVM_TARGET" ] || die "node version not retained after sourcing"
+[ "_${NVM_CURRENT#v}" = "_${NVM_TARGET}" ] || die "node version not retained after sourcing"

test/sourcing/Sourcing nvm.sh should use the default if available and no nvm node is loaded
@@ -8,21 +8,23 @@ die () { echo "$@" ; exit 1; }
 # despite being unloaded in setup, the inherited PATH still contains
 # an nvm-installed node version. We have to reset NVM_DIR after the unload.
 \. ../../nvm.sh || die 'sourcing returned nonzero exit code'
-NVM_DIR_CACHED="$NVM_DIR"
+NVM_DIR_CACHED="${NVM_DIR}"
 nvm unload || die 'unloading returned nonzero exit code'
-NVM_DIR="$NVM_DIR_CACHED"
+NVM_DIR="${NVM_DIR_CACHED}"
 
 echo '0.10.1' > ../../alias/default || die 'creation of default alias failed'
+
+which node && die 'node was found in PATH after unloading nvm'
 
 # Now to begin the real test
 \. ../../nvm.sh || die 'sourcing returned nonzero exit code'
 
 NVM_LS_CURRENT_NOT_GREPPED="$(nvm ls current | strip_colors)"
 NVM_LS_CURRENT="$(nvm ls current | strip_colors | \grep -o v0.10.1)"
-[ "_$NVM_LS_CURRENT" = '_v0.10.1' ] || die "'nvm ls current' did not return '-> v0.10.1', got '$NVM_LS_CURRENT_NOT_GREPPED'"
+[ "_${NVM_LS_CURRENT}" = '_v0.10.1' ] || die "'nvm ls current' did not return '-> v0.10.1', got '${NVM_LS_CURRENT_NOT_GREPPED}'"
 
 # NVM_LS_CURRENT_COLORED="$(nvm ls current | sed -n l)"
 
 NVM_ALIAS_DEFAULT="$(nvm alias default | strip_colors)"
-[ "_$NVM_ALIAS_DEFAULT" = "_default -> 0.10.1 (-> v0.10.1)" ] \
-  || die "'nvm alias default did not return 'default -> 0.10.1 (-> v0.10.1)', got '$NVM_ALIAS_DEFAULT'"
+[ "_${NVM_ALIAS_DEFAULT}" = "_default -> 0.10.1 (-> v0.10.1)" ] \
+  || die "'nvm alias default did not return 'default -> 0.10.1 (-> v0.10.1)', got '${NVM_ALIAS_DEFAULT}'"

test/sourcing/Sourcing nvm.sh with --install and .nvmrc should install it
@@ -13,15 +13,15 @@ fi
 echo '0.10.2' > ../../.nvmrc || die 'creation of .nvmrc failed'
 
 \. ../../nvm.sh --install
-EXIT_CODE="$(echo $?)"
+EXIT_CODE="$?"
 
 echo 'sourcing complete.'
 
-nvm_version 0.10.2 >/dev/null 2>&1 || die "v0.10.2 not installed: $(nvm ls)"
+nvm_version 0.10.2 >/dev/null 2>&1 || die "v0.10.2 not installed: \n$(nvm_ls)"
 
 [ "_$(nvm_rc_version | \grep -o -e 'with version .*$')" = "_with version <0.10.2>" ] || die "nvm_rc_version $(nvm_rc_version)"
 
-[ "_$EXIT_CODE" = "_0" ] || die "sourcing returned nonzero exit code: $EXIT_CODE"
+[ "_${EXIT_CODE}" = '_0' ] || die "sourcing returned nonzero exit code: ${EXIT_CODE}"
 
 NVM_LS_CURRENT="$(nvm ls current | \grep -o v0.10.2)"
-[ "_$NVM_LS_CURRENT" = '_v0.10.2' ] || die "'nvm ls current' did not return '-> v0.10.2', got '$NVM_LS_CURRENT' `nvm ls`"
+[ "_$NVM_LS_CURRENT" = '_v0.10.2' ] || die "'nvm ls current' did not return '-> v0.10.2', got >${NVM_LS_CURRENT}<\n$(nvm_ls)"

test/sourcing/Sourcing nvm.sh with --install should install the default
@@ -16,17 +16,17 @@ echo '0.10.2' > ../../alias/default || die 'creation of default alias failed'
 echo 'sourcing nvm with --install...'
 
 \. ../../nvm.sh --install
-EXIT_CODE="$(echo $?)"
+EXIT_CODE="$?"
 
 echo 'sourcing complete.'
 
-nvm_version 0.10.2 >/dev/null 2>&1 || die "v0.10.2 not installed: $(nvm ls)"
+nvm_version 0.10.2 >/dev/null 2>&1 || die "v0.10.2 not installed: \n$(nvm_ls)"
 
-[ "_$EXIT_CODE" = "_0" ] || die "sourcing returned nonzero exit code: $EXIT_CODE"
+[ "_$EXIT_CODE" = '_0' ] || die "sourcing returned nonzero exit code: ${EXIT_CODE}"
 
 NVM_LS_CURRENT="$(nvm ls current | strip_colors | command grep -o v0.10.2)"
-[ "_$NVM_LS_CURRENT" = '_v0.10.2' ] || die "'nvm ls current' did not return '-> v0.10.2', got '$NVM_LS_CURRENT'"
+[ "_$NVM_LS_CURRENT" = '_v0.10.2' ] || die "'nvm ls current' did not return '-> v0.10.2', >${NVM_LS_CURRENT}<\n$(nvm_ls)"
 
 NVM_ALIAS_DEFAULT="$(nvm alias default | strip_colors)"
 [ "_$NVM_ALIAS_DEFAULT" = "_default -> 0.10.2 (-> v0.10.2)" ] \
-  || die "'nvm alias default did not return 'default -> 0.10.2 (-> v0.10.2)', got '$NVM_ALIAS_DEFAULT'"
+  || die "'nvm alias default did not return 'default -> 0.10.2 (-> v0.10.2)', got >${NVM_ALIAS_DEFAULT}<\n$(nvm_ls)"