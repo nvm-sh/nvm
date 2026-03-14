nvm-sh/nvm

commit b1fa143dd8cbebd9847972c08fb383646ca00642

[Fix] `nvm_get_mirror`: ensure only a valid URL is allowed

nvm.sh
@@ -2035,11 +2035,17 @@ nvm_get_mirror() {
   esac
 
   case "${NVM_MIRROR}" in
-    *\`* | *\\* | *\'* | *\(* )
+    *\`* | *\\* | *\'* | *\(* | *' '* )
       nvm_err '$NVM_NODEJS_ORG_MIRROR and $NVM_IOJS_ORG_MIRROR may only contain a URL'
       return 2
     ;;
   esac
+
+
+  if ! nvm_echo "${NVM_MIRROR}" | command awk '{ $0 ~ "^https?://[a-zA-Z0-9./_-]+$" }'; then
+      nvm_err '$NVM_NODEJS_ORG_MIRROR and $NVM_IOJS_ORG_MIRROR may only contain a URL'
+      return 2
+  fi
 
   nvm_echo "${NVM_MIRROR}"
 }

test/fast/Unit tests/nvm_get_mirror
@@ -23,18 +23,25 @@ set -e
 [ "$(nvm_get_mirror node std)" = "https://nodejs.org/dist" ] || die "incorrect default node-std mirror"
 [ "$(nvm_get_mirror iojs std)" = "https://iojs.org/dist" ] || die "incorrect default iojs-std mirror"
 
-NVM_NODEJS_ORG_MIRROR="test://domain"
-[ "$(nvm_get_mirror node std)" = "test://domain" ] || die "node-std mirror should respect NVM_NODEJS_ORG_MIRROR"
+NVM_NODEJS_ORG_MIRROR="https://test-domain"
+[ "$(nvm_get_mirror node std)" = "https://test-domain" ] || die "node-std mirror should respect NVM_NODEJS_ORG_MIRROR"
 unset NVM_NODEJS_ORG_MIRROR
 
-NVM_IOJS_ORG_MIRROR="test://domain"
-[ "$(nvm_get_mirror iojs std)" = "test://domain" ] || die "iojs-std mirror should respect NVM_IOJS_ORG_MIRROR"
+NVM_IOJS_ORG_MIRROR="https://test-domain"
+[ "$(nvm_get_mirror iojs std)" = "https://test-domain" ] || die "iojs-std mirror should respect NVM_IOJS_ORG_MIRROR"
 unset NVM_IOJS_ORG_MIRROR
 
-NVM_NODEJS_ORG_MIRROR='`do something bad`'
-! nvm_get_mirror node std || die 'NVM_NODEJS_ORG_MIRROR errors with command injection attempt'
-[ "$(nvm_get_mirror node std)" = "" ] || die 'NVM_NODEJS_ORG_MIRROR is protected against command injection'
+testMirrors() {
+  NVM_NODEJS_ORG_MIRROR="${1-}"
+  ! nvm_get_mirror node std || die "NVM_NODEJS_ORG_MIRROR errors with command injection attempt (${1-})"
+  [ "$(nvm_get_mirror node std)" = "" ] || die 'NVM_NODEJS_ORG_MIRROR is protected against command injection'
 
-NVM_IOJS_ORG_MIRROR='`do something bad`'
-! nvm_get_mirror iojs std || die 'NVM_IOJS_ORG_MIRROR errors with command injection attempt'
-[ "$(nvm_get_mirror iojs std)" = "" ] || die 'NVM_IOJS_ORG_MIRROR is protected against command injection'
+  NVM_IOJS_ORG_MIRROR="${1-}"
+  ! nvm_get_mirror iojs std || die "NVM_IOJS_ORG_MIRROR errors with command injection attempt (${1-})"
+  [ "$(nvm_get_mirror iojs std)" = "" ] || die 'NVM_IOJS_ORG_MIRROR is protected against command injection'
+}
+
+testMirrors '`do something bad`'
+testMirrors 'https://nodejs.org/dist; xdg-open http://www.google.com;'
+testMirrors 'https://nodejs.org/dist&&xdg-open http://www.google.com;'
+testMirrors 'https://nodejs.org/dist|xdg-open http://www.google.com;'

commit 3c480159868746157417ce9cd1c1c69b76a8e7cc

[Fix] `nvm_get_mirror`: fix awk URL validation to actually reject invalid URLs
The awk expression `$0 ~ "regex"` as a bare statement in the action block evaluates the match but doesn't affect the exit code.
awk always prints the line and exits 0, making the validation a no-op.

Bug introduced in https://github.com/nvm-sh/nvm/commit/b1fa143dd8cbebd9847972c08fb383646ca00642.

nvm.sh
@@ -2247,7 +2247,7 @@ nvm_get_mirror() {
   esac
 
 
-  if ! nvm_echo "${NVM_MIRROR}" | command awk '{ $0 ~ "^https?://[a-zA-Z0-9./_-]+$" }'; then
+  if ! nvm_echo "${NVM_MIRROR}" | command awk '{ if ($0 !~ /^https?:\/\/[a-zA-Z0-9.\/_-]+$/) exit 1 }'; then
       nvm_err '$NVM_NODEJS_ORG_MIRROR and $NVM_IOJS_ORG_MIRROR may only contain a URL'
       return 2
   fi

test/fast/Unit tests/nvm_get_mirror
@@ -46,3 +46,9 @@ testMirrors '`do something bad`'
 testMirrors 'https://nodejs.org/dist; xdg-open http://www.google.com;'
 testMirrors 'https://nodejs.org/dist&&xdg-open http://www.google.com;'
 testMirrors 'https://nodejs.org/dist|xdg-open http://www.google.com;'
+
+# Test that awk URL validation rejects non-URL values
+testMirrors 'not a url'
+testMirrors 'ftp://wrong-scheme'
+testMirrors 'http://'
+testMirrors 'javascript:alert(1)'