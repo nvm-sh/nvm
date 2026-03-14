nvm-sh/nvm

commit b805de80cb8058f09fecb11786d4d2aec0dac35c

Add `PREFIX` and `NPM_CONFIG_PREFIX` env vars to `nvm debug`

nvm.sh
@@ -1446,7 +1446,9 @@ nvm() {
         setopt shwordsplit
       fi
       echo >&2 "\$SHELL: $SHELL"
-      echo >&2 "\$NVM_DIR: $(nvm_sanitize_path "$NVM_DIR")"
+      echo >&2 "\$NVM_DIR: '$(nvm_sanitize_path "$NVM_DIR")'"
+      echo >&2 "\$PREFIX: '$(nvm_sanitize_path "$PREFIX")'"
+      echo >&2 "\$NPM_CONFIG_PREFIX: '$(nvm_sanitize_path "$NPM_CONFIG_PREFIX")'"
       local NVM_DEBUG_OUTPUT
       for NVM_DEBUG_COMMAND in 'nvm current' 'which node' 'which iojs' 'which npm' 'npm config get prefix' 'npm root -g'
       do

commit 32863e1052a4705d315f985a75e51fb41e4b5141

[Fix] `nvm debug`: use default empty values for potentially unset variables
These variables may be unset, which would cause errors in shells with `set -u` (nounset) enabled.

Bugs introduced in https://github.com/nvm-sh/nvm/commit/b805de80cb8058f09fecb11786d4d2aec0dac35c and https://github.com/nvm-sh/nvm/commit/49e446dac18ce8b1267107849e6be6fe99390543.

nvm.sh
@@ -3332,10 +3332,10 @@ nvm() {
       nvm_err "\${HOME}: ${HOME}"
       nvm_err "\${NVM_DIR}: '$(nvm_sanitize_path "${NVM_DIR}")'"
       nvm_err "\${PATH}: $(nvm_sanitize_path "${PATH}")"
-      nvm_err "\$PREFIX: '$(nvm_sanitize_path "${PREFIX}")'"
-      nvm_err "\${NPM_CONFIG_PREFIX}: '$(nvm_sanitize_path "${NPM_CONFIG_PREFIX}")'"
-      nvm_err "\$NVM_NODEJS_ORG_MIRROR: '${NVM_NODEJS_ORG_MIRROR}'"
-      nvm_err "\$NVM_IOJS_ORG_MIRROR: '${NVM_IOJS_ORG_MIRROR}'"
+      nvm_err "\$PREFIX: '$(nvm_sanitize_path "${PREFIX-}")'"
+      nvm_err "\${NPM_CONFIG_PREFIX}: '$(nvm_sanitize_path "${NPM_CONFIG_PREFIX-}")'"
+      nvm_err "\$NVM_NODEJS_ORG_MIRROR: '${NVM_NODEJS_ORG_MIRROR-}'"
+      nvm_err "\$NVM_IOJS_ORG_MIRROR: '${NVM_IOJS_ORG_MIRROR-}'"
       nvm_err "shell version: '$(${SHELL} --version | command head -n 1)'"
       nvm_err "uname -a: '$(command uname -a | command awk '{$2=""; print}' | command xargs)'"
       nvm_err "checksum binary: '$(nvm_get_checksum_binary 2>/dev/null)'"