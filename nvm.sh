nvm-sh/nvm

commit 779a34e6a9adcb6528e63d595a7a8838747e7915

[Fix] `nvm_get_arch`: Rasbperry Pi: armv7l with arm64 kernel, get correct arch
Co-authored-by: Botspot <54716352+Botspot@users.noreply.github.com>
Co-authored-by: Jordan Harband <ljharb@gmail.com>
Co-authored-by: Sladyn Nunes <sladynnunes98@gmail.com>

nvm.sh
@@ -1796,6 +1796,14 @@ nvm_get_arch() {
     aarch64) NVM_ARCH="arm64" ;;
     *) NVM_ARCH="${HOST_ARCH}" ;;
   esac
+
+  # If running a 64bit ARM kernel but a 32bit ARM userland, change ARCH to 32bit ARM (armv7l)
+  L=$(ls -dl /sbin/init) #                                         if /sbin/init is 32bit executable
+  if [ "$(uname)" = "Linux" ] && [ "${NVM_ARCH}" = arm64 ] && [ "$(od -An -t x1 -j 4 -N 1 "${L#*-> }")" = ' 01' ]; then
+    NVM_ARCH=armv7l
+    HOST_ARCH=armv7l
+  fi
+
   nvm_echo "${NVM_ARCH}"
 }
 

test/fast/Unit tests/nvm_get_arch
@@ -79,4 +79,6 @@ run_test amd64 smartos x64 no_pkg_info
 run_test x86 osx x86
 run_test amd64 osx x64
 
+run_test arm64 smartos x64
+
 cleanup

commit a5de7236d638afef725c4dcd87cb662c1a55b019

[Fix] `nvm_get_arch`: add `command` prefix to `uname` call
Other `uname` calls in the file use `command uname` to bypass any user aliases or functions.
This one was missing it.

Bug introduced in https://github.com/nvm-sh/nvm/commit/779a34e6a9adcb6528e63d595a7a8838747e7915 / #2469.

nvm.sh
@@ -2162,7 +2162,7 @@ nvm_get_arch() {
 
   # If running a 64bit ARM kernel but a 32bit ARM userland,
   # change ARCH to 32bit ARM (armv7l) if /sbin/init is 32bit executable
-  if [ "$(uname)" = "Linux" ] \
+  if [ "$(command uname)" = "Linux" ] \
     && [ "${NVM_ARCH}" = arm64 ] \
     && [ "$(command od -An -t x1 -j 4 -N 1 "/sbin/init" 2>/dev/null)" = ' 01' ]\
   ; then